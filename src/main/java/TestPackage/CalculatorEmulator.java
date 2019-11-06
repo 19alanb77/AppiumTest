f1d1eb8bf38c068d2776d7310e0603daf65a11fd
Alan Buda <alan.buda@accenture.com>
2019-11-06 14:20:08 +0100

    github backup

---
+++ src/main/java/TestPackage/CalculatorEmulator.java
@@ -0,0 +1,50 @@
+package TestPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+
+public class CalculatorEmulator 
+{
+	private static AndroidDriver driver;
+	
+	@BeforeClass(description = "Initialize driver with specified properties")
+	public void setup() throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("noReset", "true");
+		capabilities.setCapability("fullReset", "false");
+		capabilities.setCapability("deviceName", "Android Emulator");
+		capabilities.setCapability("platformVersion", "9.0");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("appPackage", "com.android.calculator2");
+		capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+		
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4727/wd/hub"),capabilities);
+	}
+	
+	@Test(description = "Add two digits and check result")
+	public void calculatorTest() {
+	    WebElement six = driver.findElementById("com.android.calculator2:id/digit_6");
+	    WebElement two = driver.findElementById("com.android.calculator2:id/digit_2");
+	    WebElement plus = driver.findElementById("com.android.calculator2:id/op_add");
+        WebElement equalTo = driver.findElementById("com.android.calculator2:id/eq");
+	    WebElement results = driver.findElementById("com.android.calculator2:id/formula");
+	        
+	    six.click();
+	    plus.click();
+	    two.click();
+	    equalTo.click();
+	    Assert.assertTrue(results.getText().equals("8"));
+	}
+	
+	@AfterClass(description = "Close initialized driver")
+	public void closeDriver() {
+	    driver.quit();
+	}
+}
---
+++ src/main/java/TestPackage/WebEmulator.java
@@ -0,0 +1,41 @@
+package TestPackage;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+
+public class WebEmulator 
+{
+	private static AndroidDriver driver;
+	
+	@BeforeClass(description = "Initialize driver with specified properties")
+	public void setup() throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("noReset", "true");
+		capabilities.setCapability("fullReset", "false");
+		capabilities.setCapability("deviceName", "Android Emulator");
+		capabilities.setCapability("platformVersion", "9.0");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("appPackage", "com.android.chrome");
+		capabilities.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+		
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4727/wd/hub"),capabilities);
+	}
+	
+	@Test(description = "Try to login to facebook with random name")
+	public void calculatorTest() {
+		driver.findElementByXPath("//*[@id='m_login_email']").sendKeys("your_email@gmail.com");
+		driver.findElementByXPath("//*[@id='m_login_password']").sendKeys("your_password");
+		driver.findElementByXPath("//*[@id=\"u_0_6\"]").click();
+	}
+	
+	@AfterClass(description = "Close initialized driver")
+	public void closeDriver() {
+	    driver.quit();
+	}
+
+}
